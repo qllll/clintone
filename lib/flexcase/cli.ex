c2d4a33a63bb6e2a3694a68ef8b080a5a29e0e97
qllll <q@ll.ll>
2016-10-29 23:28:54 +0900

    process

--- lib/flexcase/cli.ex
+++ lib/flexcase/cli.ex
@@ -2,6 +2,7 @@ defmodule Flexcase.CLI do
   def run(argv) do
     argv
     |> parse_args
+    |> process
   end
 
   def parse_args(argv) do
@@ -15,5 +16,30 @@ defmodule Flexcase.CLI do
       { [ camel: true ], [ words ], _ } -> { :camel, words }
       _ -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage:
+      flexcase -s "foo\\nbar"
+      flexcase -c "foo\\nbar"
+    """
+    System.halt(0)
+  end
+
+  def process({:snake, words}) do
+    do_process(&Inflex.underscore/1, words)
+  end
+
+  def process({:camel, words}) do
+    do_process(&(Inflex.camelize(&1, :lower)), words)
+  end
+
+  defp do_process(inflex, words) do
+    words
+    |> String.split("\n")
+    |> Enum.map(inflex)
+    |> Enum.join("\n")
+    |> IO.puts
   end
 end
